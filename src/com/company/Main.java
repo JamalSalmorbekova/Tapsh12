5aedc5e55b0cd06d5507127c288084637674703a
Jamal <jamalpeaksoft@gmail.com>
2022-02-24 12:19:00 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Treugolnik uchburchtuk = new Treugolnik();
+    uchburchtuk.a = 7;
+    uchburchtuk.b = 4;
+    uchburchtuk.c = 11;
+
+
+        System.out.print("Уч бурчтуктун  аянты = ");
+        uchburchtuk.area();
+    }
+}
---
+++ src/com/company/Treugolnik.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Treugolnik {
+    int a;
+    int b;
+    int c;
+
+    public  void  area(){
+        System.out.println( (a*b*c)/2);
+//  уч бурчтуктун  шаблонун  туздук     /
+    }
+}
